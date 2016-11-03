3a59db38789f72bfdcb6d3fe40e792cdc857929c
Bell, Ian (Fed) <ian.bell@nist.gov>
2016-11-03 14:50:11 -0600

    Updates to fitter_py.py - better error capturing, etc.

--- fitter_py.py
+++ fitter_py.py
@@ -21,9 +21,6 @@ from deap_optimize import minimize_deap
 #             Instantiation
 #--------------------------------------
 
-with open('fit0.json','r') as fp:
-    departure0 = json.load(fp)
-
 def get_data():
 
     Smolen_JSON = open('data/ammonia-water/PTXY-Smolen.json','r').read()
@@ -45,8 +42,8 @@ def get_data():
     print ('# "data" points (+ constraints):', len(all_JSON_data['data']))
     return all_JSON_data
 
-Nterms = 3 # Total number of terms in the summation of terms
-Npoly = 1 # Number of "polynomial" terms of the form n_i*tau^t_u*delta^d_i*exp(-cdelta_i*delta^ldelta_i-ctau_i*tau^ltau_i)
+Nterms = 6 # Total number of terms in the summation of terms
+Npoly = 4 # Number of "polynomial" terms of the form n_i*tau^t_u*delta^d_i*exp(-cdelta_i*delta^ldelta_i-ctau_i*tau^ltau_i)
 Nexp_terms = 3 # Number of terms in each of the inner summations over j for i-th term
 Nexp = Nterms - Npoly # Number of "exponential" terms with the full polynomial term in the exponential
 Nu = (Nexp*Nexp_terms + Npoly) # Number of total terms associated with one of the things in the exp(u) function
@@ -57,9 +54,9 @@ n_bounds = [(-5, 5)]*Nterms
 t_bounds = [(0.25, 20)]*Nterms
 d_bounds = [(1, 5)]*Nterms
 ldelta_bounds = [(1,5)]*Nu
-cdelta_bounds = [(-5,0)]*Nu
-ltau_bounds = [(1,5)]*Nu
-ctau_bounds = [(-5,0)]*Nu
+cdelta_bounds = [(-5,5)]*Nu
+ltau_bounds = [(0.1,5)]*Nu
+ctau_bounds = [(-5,5)]*Nu
 bounds = coeffs_bounds + n_bounds + t_bounds + d_bounds + ldelta_bounds + cdelta_bounds + ltau_bounds + ctau_bounds
 
 # Definition of how the array of coefficients should be partitioned into chunks 
@@ -71,20 +68,34 @@ generator_functions = [random.uniform]*(4 + 2*Nterms) + [random.randint]*(Nterms
 normalizing_functions = [lambda x: x]*(4 + 2*Nterms) + [lambda x: min(max(int(round(x)), 1), 5) ]*(Nterms + Nu) + [lambda x: x]*Nu*3
 sigma = [0.01]*(4 + 2*Nterms) + [1]*(Nterms + Nu) + [0.01]*Nu*3
 
+# generator_functions = [random.uniform]*(4 + 2*Nterms) + [random.uniform]*(Nterms + Nu) + [random.uniform]*Nu*3
+# normalizing_functions = [lambda x: x]*(4 + 2*Nterms) + [lambda x: x]*(Nterms + Nu) + [lambda x: x]*Nu*3
+# sigma = [0.01]*(4 + 2*Nterms) + [0.01]*(Nterms + Nu) + [0.01]*Nu*3
+
+# A couple of sanity checks
 assert(len(bounds) == len(generator_functions))
 assert(len(bounds) == len(normalizing_functions))
 assert(len(bounds) == len(sigma))
+
+with open('EWL_NH3_H2O.json','r') as fp:
+    departure0 = json.load(fp)
 
 # Instantiate the fitter class with the experimental data stored in JSON format
 cfc = MCF.CoeffFitClass(json.dumps(get_data()))
 cfc.setup(json.dumps(departure0.copy()))
 
-# Instantiate the struct holding coefficients for the departure function
-coeffs = MCF.Coefficients()
+cfc.evaluate_serial([0.911640, 0.9111660, 1.0541730, 1.3223907])
+jj = json.loads(cfc.dump_outputs_to_JSON())
+with open('baseline.json', 'w') as fp:
+    fp.write(json.dumps(jj, indent = 2))
+print('baseline:', cfc.sum_of_squares()**0.5)
+#sys.exit()
 
 def objective(x, cfc, Nterms, fit_delta = True, x0 = None, write_JSON = False):
     if isinstance(x, np.ndarray):
         x = x.tolist()
+    # Instantiate the struct holding coefficients for the departure function
+    coeffs = MCF.Coefficients()  
     
     # Deconstruct the array into the data structures passed to the C++ class
     betagamma, coeffs.n, coeffs.t, coeffs.d, coeffs.ldelta, coeffs.cdelta, coeffs.ltau, coeffs.ctau = ArrayDeconstructor(xdims, x)
@@ -99,8 +110,11 @@ def objective(x, cfc, Nterms, fit_delta = True, x0 = None, write_JSON = False):
     cfc.setup(coeffs)
 
     try:
+        # cfc.evaluate_serial(betagamma)
         cfc.evaluate_parallel(betagamma, 4)
         err = cfc.sum_of_squares()**0.5
+        if np.isnan(err):
+            err = 1e8
         if err < 0.1 or write_JSON:
             jj = json.loads(cfc.dump_outputs_to_JSON())
             with open(str(err).replace('.','_') + '.json', 'w') as fp:
@@ -138,7 +152,7 @@ start_time = time.clock()
 print('About to fit', len(bounds), 'coefficients')
 
 # Minimize using deap (global optimization using evolutionary optimization)
-results = minimize_deap(objective, bounds, Nindividuals=140000, Ngenerations=20, Nhof = 50, args=(cfc, Nterms), 
+results = minimize_deap(objective, bounds, Nindividuals= 25*len(bounds), Ngenerations=40, Nhof = 50, args=(cfc, Nterms), 
                         generator_functions=generator_functions, normalizing_functions=normalizing_functions, sigma=sigma)
 
 # Print elapsed time for this first global optimization
@@ -148,7 +162,7 @@ print(time.clock()-start_time, 's for deap optimization')
 with open('hof.json', 'w') as fp:
    fp.write(json.dumps([{'c': list(ind), 'fitness': ind.fitness.values} for ind in results['hof']], indent = 2))
 
-# Load the HOF back in from file (this serves as rough serialization)
+# Load the HOF back in from file (this write/load serves as rough serialization and checkpointing)
 hof = json.load(open('hof.json','r'))
 
 # ****************
