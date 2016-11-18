3caef4818b3a74e52b1a711b1646944cba19a641
Bell, Ian (Fed) <ian.bell@nist.gov>
2016-11-18 10:46:03 -0700

    Print out a bit more info on the fits than before

--- eval_solns.py
+++ eval_solns.py
@@ -2,11 +2,17 @@
 from __future__ import print_function
 import matplotlib.pyplot as plt, pandas, json, glob, sys, numpy as np
 
-print('name\t Npoints \t residual\t MARE(rho)[%]\tr(PTXY)')
-for soln in glob.glob('soln*.json') + ['baseline.json']:
+#for soln in glob.glob('soln*.json') + ['baseline.json', 'EthanolWaterFit.json']:
+results = []
+for soln in ['baseline.json', 'GeneralizedAirWaterFit.json']:
 	data = json.loads(open(soln,'r').read())['data']
 	df = pandas.DataFrame(data)
 	PRhoT = df[df['type'] == 'PRhoT']
 	PTXY = df[df['type'] == 'PTXY']
 	df.to_excel(soln.rsplit('.')[0]+'.xlsx')
-	print(soln, len(df), ((df['residue']**2).sum())**0.25, np.mean(np.abs(PRhoT['residue']))*100, np.mean(np.abs(PTXY['residue']))*100)
+	R = ((df['residue']**2).sum())**0.25
+	results.append(dict(fname=soln, Np=len(df), residue=R, 
+		                 mean_abs_PRhoT_res=np.mean(np.abs(PRhoT['residue']))*100, mean_abs_PTXY_res=np.mean(np.abs(PTXY['residue'])),
+	                 	 max_abs_PRhoT_res=np.max(np.abs(PRhoT['residue']))*100, max_abs_PTXY_res=np.max(np.abs(PTXY['residue']))
+	                 	))
+print(pandas.DataFrame(results))
