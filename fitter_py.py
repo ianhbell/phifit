f57149439aef3274f30c99b72ffa2f8b334829aa
Bell, Ian (Fed) <ian.bell@nist.gov>
2016-11-18 12:24:49 -0700

    Update fitter code (problems with PT flashes, as usual)

--- fitter_py.py
+++ fitter_py.py
@@ -27,10 +27,13 @@ def get_data():
     Smolen_data = json.loads(Smolen_JSON)
     Harms_JSON = open('data/ammonia-water/Harms-Watzenberg.json','r').read()
     Harms_data = json.loads(Harms_JSON)
+    Muromachi_JSON = open('data/ammonia-water/PVT-Muromachi.json','r').read()
+    Muromachi_data = json.loads(Muromachi_JSON)
 
     all_JSON_data = {'about': Harms_data['about'], 'data': []}
     all_JSON_data['data'] += Smolen_data['data']
     all_JSON_data['data'] += Harms_data['data']
+    all_JSON_data['data'] += Muromachi_data['data']
 
     random.shuffle(all_JSON_data['data'])
 
@@ -42,11 +45,12 @@ def get_data():
     print ('# "data" points (+ constraints):', len(all_JSON_data['data']))
     return all_JSON_data
 
-Nterms = 6 # Total number of terms in the summation of terms
-Npoly = 6 # Number of "polynomial" terms of the form n_i*tau^t_u*delta^d_i*exp(-cdelta_i*delta^ldelta_i-ctau_i*tau^ltau_i)
-Nexp_terms = 5 # Number of terms in each of the inner summations over j for i-th term
+Nterms = 10 # Total number of terms in the summation of terms
+Npoly = 10 # Number of "polynomial" terms of the form n_i*tau^t_u*delta^d_i*exp(-cdelta_i*delta^{ldelta_i}-ctau_i*tau^{ltau_i})
+Nexp_terms = 0 # Number of terms in each of the inner summations over j for i-th term
 Nexp = Nterms - Npoly # Number of "exponential" terms with the full polynomial term in the exponential
-Nu = (Nexp*Nexp_terms + Npoly) # Number of total terms associated with one of the things in the exp(u) function
+Nu = 0#(Nexp*Nexp_terms + Npoly) # Number of total terms associated with one of the things in the exp(u) function
+assert(Npoly <= Nterms)
 
 # Bounds for the random generation for each family of parameter
 coeffs_bounds = [(0.1, 1.5)]*4
@@ -66,7 +70,7 @@ xdims = [4, Nterms, Nterms, Nterms, xu, xu, xu, xu]
 
 generator_functions = [random.uniform]*(4 + 2*Nterms) + [random.randint]*(Nterms + Nu) + [random.uniform]*Nu*3
 normalizing_functions = [lambda x: x]*(4 + 2*Nterms) + [lambda x: min(max(int(round(x)), 1), 5) ]*(Nterms + Nu) + [lambda x: x]*Nu*3
-sigma = [0.01]*(4 + 2*Nterms) + [1]*(Nterms + Nu) + [0.01]*Nu*3
+sigma = [0.5]*(4 + 2*Nterms) + [1]*(Nterms + Nu) + [0.5]*Nu*3
 
 # generator_functions = [random.uniform]*(4 + 2*Nterms) + [random.uniform]*(Nterms + Nu) + [random.uniform]*Nu*3
 # normalizing_functions = [lambda x: x]*(4 + 2*Nterms) + [lambda x: x]*(Nterms + Nu) + [lambda x: x]*Nu*3
@@ -95,6 +99,25 @@ with open('w_fitting_betasgammas.json', 'w') as fp:
     fp.write(json.dumps(jj, indent = 2))
 print('w/ fitting betas, gammas:', cfc.sum_of_squares()**0.5)
 
+cfc.set_departure_function_by_name("GeneralizedAirWater")
+cfc.set_binary_interaction_double(0,1,"Fij",0.487755102041)
+cfc.evaluate_serial([0.9574512152877733, 1.049955858771539, 0.9923911123595757, 0.8379666315254417])
+jj = json.loads(cfc.dump_outputs_to_JSON())
+with open('GeneralizedAirWaterFit.json', 'w') as fp:
+    fp.write(json.dumps(jj, indent = 2))
+    sys.exit()
+
+# for fcn in ['Ethanol-Water','Methane-Nitrogen','Methane-CarbonDioxide','Methane-Ethane','Methane-Propane','Nitrogen-CarbonDioxide','Nitrogen-Ethane','Methane-Hydrogen','GeneralizedAlkane','CarbonDioxide-Water','GeneralizedAirWater','Argon-CarbonDioxide']:
+# for fcn in ['Ethanol-Water','GeneralizedAirWater', 'CarbonDioxide-Water']:
+#     for Fij in np.linspace(0.1, 2):
+#         cfc.set_departure_function_by_name(fcn)
+#         cfc.set_binary_interaction_double(0,1,"Fij",Fij)
+#         cfc.run(True, 4, [0.911640, 0.9111660, 1.0541730, 1.3223907])
+#         cfc.dump_outputs_to_JSON()
+#         print(Fij, 'w/ ['+fcn+'] and fitting betas, gammas:', cfc.sum_of_squares()**0.5, cfc.cfinal())
+# sys.exit()
+
+# cfc.setup(json.dumps(departure0.copy()))
 
 def objective(x, cfc, Nterms, fit_delta = True, x0 = None, write_JSON = False):
     if isinstance(x, np.ndarray):
@@ -157,11 +180,11 @@ start_time = time.clock()
 print('About to fit', len(bounds), 'coefficients')
 
 # Minimize using deap (global optimization using evolutionary optimization)
-results = minimize_deap(objective, bounds, Nindividuals= 250*len(bounds), Ngenerations=40, Nhof = 50, args=(cfc, Nterms), 
+results = minimize_deap(objective, bounds, Nindividuals= 50*len(bounds), Ngenerations=40, Nhof = 50, args=(cfc, Nterms), 
                         generator_functions=generator_functions, normalizing_functions=normalizing_functions, sigma=sigma)
 
 # Print elapsed time for this first global optimization
-print(time.clock()-start_time, 's for deap optimization')
+print((time.clock()-start_time)/3600.0, 'h for deap optimization')
 
 # Serialize the hall of fame
 with open('hof.json', 'w') as fp:
@@ -196,4 +219,4 @@ for iind, soln in enumerate(hof):
         fp.write(json.dumps(json.loads(cfc.dump_outputs_to_JSON()), indent=2))
 
 # Print out total elapsed time
-print(time.clock()-start_time, 's in total')
+print((time.clock()-start_time)/3600.0, 'h in total')
